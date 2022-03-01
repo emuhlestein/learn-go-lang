d32d14fc7dc0918b7e3eee88a9cab458ec6a1830
emuhlestein <emuhlestein@yahoo.com>
2022-03-01 04:13:15 -0700

    Save changes

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ git branch -m main
 git branch --set-upstream-to=origin/main main  
 git pull --allow-unrelated-histories  
 
+GO is statically typed. 
+All variables are initilized.
+Zero Values:
+- numeric types: 0
+- bool type: false
+- string type: "" (empty string)
+- pointer type: nill
+
+// you must provide a type for each variable you declare or Go should be able to infer it
+
+age := 18
+_ = age // blank variable removes unused var error
+
--- src/simple/main.go
+++ src/simple/main.go
@@ -8,6 +8,26 @@ import (
 
 // Test
 func main() {
+	// var age int = 50	// full declaration
+	var age = 50 // don't need type-go can infer type from assigned value
+
 	var num int = numbers.GetNumberFive()
-	fmt.Printf("Hello World: %d", num)
+	fmt.Printf("Hello World: %d %d\n", num, age)
+
+	// short declaration operator-short hand method for variable declaration
+	// only works in block scope
+	s := "Learning Go-Lang"
+	fmt.Println(s)
+
+	// blank variable-declare a variable without using it
+	_ = "a value"
+
+	car, cost := "Toyota", 25000
+	fmt.Println(car, cost)
+
+	var a = 4
+	var b = 5.2
+	// a = b can't do this
+	a = int(b)
+	fmt.Println(a, b)
 }
